d43e6f63d051b1b9bdba9103edf485a6ec0bb389
Kevin Brown <chevinbrown@gmail.com>
2017-07-14 09:58:29 -0500

    Added test post MVC & removed rake task

---
+++ test/dummy/app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %w(show)
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      respond_with(@post)
+    else
+      respond_with(@post, status: 422)
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
